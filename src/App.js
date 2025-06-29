ac1699aa787f500fbcee0d77d7ca6affcda857ca
ROHITH VUSHAGONI <vushagonirohith123@gmail.com>
2025-06-29 19:34:51 +0530

    erripukgovardhan

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import Dashboard from './components/Dashboard';
-import ExpenseList from './components/ExpenseList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 import AddExpense from './components/AddExpense';
-import './App.css';
+import ExpenseList from './components/ExpenseList';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Expense Tracker</h1>
-      <AddExpense />
-      <ExpenseList />
-      <Dashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/expenses" element={<AddExpense />} />
+        <Route path="/list" element={<ExpenseList />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
